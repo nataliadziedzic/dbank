9cfae69df4fbd287515fb94cf0c95118b681d304
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-10-19 08:23:38 +0200

    Add missing packages and configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,11 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-waffle';
 import secret from './environment/secrets.json'
 
-module.exports = {
-  solidity: '0.8.4',
+export default {
+  solidity: '0.8.7',
   paths: {
     sources: './blockchain/contracts',
     tests: './blockchain/tests',
