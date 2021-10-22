24be1dd7476a52a1c40eca35d0888cd6194f1d86
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-10-22 08:30:57 +0200

    Track account changing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,18 @@ import React, { useEffect } from 'react'
 import Bank from '../Bank/Bank'
 
 const App = () => {
+  const ethereum = (window as any).ethereum;
+  useEffect(() => {
+      if (ethereum) {
+        ethereum.on('accountsChanged', () => {
+          window.location.reload()
+        })
+        ethereum.on('chainChanged', () => {
+          window.location.reload()
+        })
+      }
+  }, [ethereum])
+
   return (
     <div className='App'>
       <header className='header'>DBank</header>
